9797e62c938379f9a173b34e034d9fd1f366a785
Callum Oakley <hello@callumoakley.net>
2020-12-15 11:53:02 +0000

    transient vec instead of a map for a bit of a speed boost

--- src/day_15.clj
+++ src/day_15.clj
@@ -2,37 +2,35 @@
   (:require
     [clojure.test :refer [deftest is]]))
 
-(def data
-  [0 14 6 20 1 4])
-
 ;; Van Eck's sequence https://youtu.be/etMJxB-igrc
 (defn turn [[prev i mem]]
   (let [seen (get mem prev)
         age (if seen (- i seen) 0)]
-    [age (inc i) (assoc mem prev i)]))
+    [age (inc i) (assoc! mem prev i)]))
 
-(defn game [seed]
-  (->> (iterate turn
-         [(last seed)
-          (dec (count seed))
-          (into {} (map-indexed (fn [i n] [n i]) seed))])
-    rest
-    (map first)
-    (concat seed)))
-
-(defn part-1 [seed]
-  (nth (game seed) 2019))
-
-(defn part-2 [seed]
-  (nth (game seed) 29999999))
+(defn game [seed n]
+  (nth
+    (->> (iterate turn
+           [(last seed)
+            (dec (count seed))
+            (transient
+              (apply assoc (vec (repeat n nil))
+                (flatten (map-indexed (fn [i m] [m i]) (drop-last seed)))))])
+      rest
+      (map first)
+      (concat seed))
+    (dec n)))
 
 (def sample
   [0 3 6])
 
+(def data
+  [0 14 6 20 1 4])
+
 (deftest test-part-1
-  (is (= (part-1 sample) 436))
-  (is (= (part-1 data) 257)))
+  (is (= (game sample 2020) 436))
+  (is (= (game data 2020) 257)))
 
 (deftest test-part-2
-  (is (= (part-2 sample) 175594))
-  (is (= (part-2 data) 8546398)))
+  (is (= (game sample 30000000) 175594))
+  (is (= (game data 30000000) 8546398)))
