53d2c76fde072a33df3e1788f129c6ba32112638
Callum Oakley <hello@callumoakley.net>
2020-12-23 17:01:52 +0000

    speedup using a vec instead of a map

--- src/day_23.clj
+++ src/day_23.clj
@@ -4,30 +4,35 @@
     [clojure.test :refer [deftest is]]))
 
 (defn initial-state [cups]
-  {:cups (transient
-           (into {(last cups) (first cups)} (map vec (partition 2 1 cups))))
+  {:next-cup (->> (partition 2 1 cups)
+               (concat [[0 nil] [(last cups) (first cups)]])
+               (sort-by first)
+               (mapv second)
+               transient)
    :current (first cups)
    :maxcup (apply max cups)})
 
-(defn move [{:keys [cups current maxcup] :as state}]
+(defn move [{:keys [next-cup current maxcup] :as state}]
   ;; ... current a b c d ... destination e ...
-  (let [a (cups current) b (cups a) c (cups b) d (cups c)
+  (let [a (next-cup current) b (next-cup a) c (next-cup b) d (next-cup c)
         destination (->> (iterate #(if (= % 1) maxcup (dec %)) current)
                       rest
                       (some #(when (not (#{a b c} %)) %)))
-        e (cups destination)]
-    (assoc state :cups (assoc! cups current d destination a c e) :current d)))
+        e (next-cup destination)]
+    (assoc state
+      :next-cup (assoc! next-cup current d destination a c e)
+      :current d)))
 
 (defn game [n cups]
-  (first (drop n (iterate move (initial-state cups)))))
+  (:next-cup (first (drop n (iterate move (initial-state cups))))))
 
 (defn part-1 [cups]
-  (let [cups* (:cups (game 100 cups))]
-    (str/join (take (dec (count cups)) (rest (iterate cups* 1))))))
+  (let [next-cup (game 100 cups)]
+    (str/join (take (dec (count cups)) (rest (iterate next-cup 1))))))
 
 (defn part-2 [cups]
-  (let [cups* (:cups (game 10000000 (concat cups (range 10 1000001))))
-        a (cups* 1) b (cups* a)]
+  (let [next-cup (game 10000000 (concat cups (range 10 1000001)))
+        a (next-cup 1) b (next-cup a)]
     (* a b)))
 
 (def sample
